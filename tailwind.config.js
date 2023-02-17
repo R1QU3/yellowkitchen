2b1e52eace0f2c89c2973f65f3c7266fdb9db3fe
r1qu3 <josehenriquehyttspf@gmail.com>
2023-02-17 16:50:21 -0300

    feat: add base project website

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'display': ['Poppins', 'sans-serif'],
+      'body': ['Martel Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
